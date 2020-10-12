82037523b574ec5ac5aa7a85803b72c7e746c6e0
danubiobwm <danubio.bwm@gmail.com>
2020-10-12 18:30:04 -0300

    maps

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
+import mapMarkerImg from "../images/map-marker.svg";
+import { Map, TileLayer } from "react-leaflet";
 
-function OrphanagesMap(){
-  return(
-    <h1>Hello orf</h1>
-  )
+import 'leaflet/dist/leaflet.css'
+
+import "../styles/pages/orphanages-map.css";
+
+function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="local" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+        <footer>
+          <strong>Juazeiro do Norte</strong>
+          <span>Ceará</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-7.1856939, -39.3504155]}
+        zoom={12}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+      <Link to="#" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
